144f414219c50c3351c0abb968cb59f08ff2bde1
Khoa Nguyen <khoa.nguyen@netapp.com>
2025-03-15 19:44:31 +0700

    cmp keymap

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -32,7 +32,7 @@ return {
           ["<C-d>"] = cmp.mapping.scroll_docs(5),
           ["<C-space>"] = cmp.mapping.complete(),
           ["<C-e>"] = cmp.mapping.abort(),
-          ["<cr>"] = cmp.mapping.confirm({ select = true }),
+          ["<tab>"] = cmp.mapping.confirm({ select = true }),
         }),
         sources = cmp.config.sources({ { name = "nvim_lsp" }, }, { { name = "buffer" }, })
       })
