fbd329aba2003c1800f77c6c90482dda07567190
Khoa Nguyen <khoa.nguyen@netapp.com>
2025-03-18 09:41:37 +0700

    remove bar

--- sketchybar/plugins/net.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-LABEL=$(ipconfig getsummary en0 | awk -F ' SSID : '  '/ SSID : / {print $2}')
-sketchybar --set $NAME label=$LABEL
--- sketchybar/plugins/ram.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-LABEL=$(memory_pressure | grep "System-wide memory free percentage:" | awk '{print "RAM:"100-$5"%"}')
-sketchybar --set $NAME label=$LABEL
