ad6bae02611a5e9c71537765d9d910e9d88f73fa
Khoa Nguyen <khoa.nguyen@netapp.com>
2025-04-17 15:45:48 +0700

    check firefox audio as well

--- bin/autolock.sh
+++ bin/autolock.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/bash
-players=('vlc' 'mpv' 'smplayer' 'mplayer')
-
+# check if the browser is currently playing audio
+if pgrep -x 'firefox' > /dev/null; then
+  if pactl list short sink-inputs | grep -q 'firefox'; then
+    exit 0
+  fi
+fi
 # check if any of the video players are running
+players=('vlc' 'mpv' 'smplayer' 'mplayer')
 for player in "${players[@]}"; do
   if pgrep -x "$player" > /dev/null; then
     exit 0
