1309bc3e1aa0a3ecf81f3ba245ba2330827c95c1
Khoa Nguyen <khoanguyenx@icloud.com>
2025-04-20 21:54:26 +0700

    add redshift

---
+++ bin/redshift.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+# vim: ft=bash
+if pgrep -x 'redshift' > /dev/null; then
+  pkill redshift &
+  notify-send 'redshift disabled'
+else
+  redshift redshift -l 10.8:106.6 &
+  notify-send 'redshift enabled'
+fi
