23f8bdfaf5b09535ac553be4e3086265184907c2
Khoa Nguyen <khoanguyenx@icloud.com>
2025-04-25 09:42:44 +0700

    some tweaks

--- bin/redshift.sh
+++ bin/redshift.sh
@@ -4,6 +4,6 @@ if pgrep -x 'redshift' > /dev/null; then
   pkill redshift &
   notify-send 'redshift disabled'
 else
-  redshift redshift -l 10.8:106.6 &
+  redshift -l 10.8:106.6 &
   notify-send 'redshift enabled'
 fi
