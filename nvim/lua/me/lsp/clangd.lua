49c9374a66f856378d16bcff670e5cd63ca9b0ce
Khoa Nguyen <khoa.nguyen@netapp.com>
2025-05-15 10:52:11 +0700

    refactor

---
+++ nvim/lua/me/lsp/clangd.lua
@@ -0,0 +1,8 @@
+vim.lsp.config("clangd", {
+    cmd = { "clangd" },
+    filetypes = { "c", "cpp", "objc", "objcpp", "cuda", "proto" },
+    root_markers = {
+        ".clangd", ".clang-tidy", ".clang-format", "compile_commands.json",
+        "compile_flags.txt", "configure.ac", ".git"
+    }
+})
